203f6ff6061495f7b49a43d51388fa3c9e343bc3
sjukica <46380665+sjukica@users.noreply.github.com>
2019-05-01 18:51:52 +0200

    Add files via upload

---
+++ Samoljubav/navigation.php
@@ -0,0 +1,12 @@
+<div class="sljbanner"></div>
+<nav>
+	<ul>
+		<li><a href="index.php?navigation=1">Home</a></li>
+		<li><a href="index.php?navigation=2">O nama</a></li>
+		<li><a href="index.php?navigation=3">Novosti</a></li>
+		<li><a href="index.php?navigation=4">Kontakt</a></li>
+		<li><a href="index.php?navigation=5">Galerija</a></li>
+
+
+	</ul>
+</nav>
---
+++ Samoljubav/novosti.php
@@ -0,0 +1,18 @@
+<h1>Novosti</h1>
+	<div class="novosti">
+			<a href="novost-1.html"><img src="novosti/majica1.jpg" alt="Majica - bijela" title="Majica - bijela"></a>
+			<h2>Samoljubav majica dobila nagradu</h2>
+			<p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy <a href="novost-1.html">Više ...</a></p>
+			<p><time datetime="2019-04-25">25.04.2019.</time></p>
+			<hr>
+			<a href="novost-2.html"><img src="novosti/majica2.jpg" alt="Majica - crna" title="Majica - crna"></a>
+			<h2>Nove metode šivanja pamuka</h2>
+			<p>Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. <a href="novost-2.html">Više ...</a></p>
+			<p><time datetime="2019-02-11">11.10.2019.</time></p>
+			<hr>
+			<a href="novost-3.html"><img src="novosti/majica3.jpg" alt="Majica - military" title="Majica - military"></a>
+			<h2>Zašto su majice najbolje</h2>
+			<p>ll the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. <a href="novost-3.html">Više ...</a></p>
+			<p><time datetime="2019-03-17">17.03.2019.</time></p>
+			<hr>
+		</div>
